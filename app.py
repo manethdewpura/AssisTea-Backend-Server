e1d8aff680055283bcb61546bb31131619dc0539
Maneth Dewpura <dewpuramaneth@gmail.com>
2025-10-11 15:14:19 +0530

    Initial Flask backend commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({"message": "Research backend running on laptop"})
+
+@app.route('/sensor', methods=['POST'])
+def receive_sensor_data():
+    data = request.json
+    print("Received data:", data)
+    return jsonify({"status": "success", "received": data}), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
